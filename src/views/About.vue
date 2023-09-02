c7df98b93473f0a9c335a7d497b64896b5a8fa58
Jalal Habeeb <jalalhabeeb@gmail.com>
2023-09-02 10:21:44 +0200

    add routes and json server

---
+++ src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur in
+      adipisci aliquam unde, eius eaque natus. Provident tempora deserunt iure
+      corrupti repellendus expedita ex qui! Eius beatae cum accusamus dolorum?
+      Exercitationem possimus unde obcaecati reprehenderit rerum aliquid
+      deserunt excepturi, dolorum dolore enim non placeat ipsa dolorem aliquam,
+      nihil inventore dolores illum fugiat fuga assumenda? Voluptatibus sequi
+      quos fuga minus velit, animi fugit corporis a blanditiis? Omnis,
+      distinctio? Debitis magni maxime, iste numquam atque voluptas ut
+      laboriosam alias? Ratione in delectus tenetur tempore esse, repellendus
+      accusantium quasi dicta sunt quia iure nisi temporibus, doloremque
+      explicabo quos facere omnis eos. Ea, porro?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur in
+      adipisci aliquam unde, eius eaque natus. Provident tempora deserunt iure
+      corrupti repellendus expedita ex qui! Eius beatae cum accusamus dolorum?
+      Exercitationem possimus unde obcaecati reprehenderit rerum aliquid
+      deserunt excepturi, dolorum dolore enim non placeat ipsa dolorem aliquam,
+      nihil inventore dolores illum fugiat fuga assumenda? Voluptatibus sequi
+      quos fuga minus velit, animi fugit corporis a blanditiis? Omnis,
+      distinctio? Debitis magni maxime, iste numquam atque voluptas ut
+      laboriosam alias? Ratione in delectus tenetur tempore esse, repellendus
+      accusantium quasi dicta sunt quia iure nisi temporibus, doloremque
+      explicabo quos facere omnis eos. Ea, porro?
+    </p>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <h1>Home Page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi vitae
+      placeat dolore minus sint voluptatem ipsam, unde officiis quisquam
+      possimus vel ea magnam necessitatibus dolores facilis mollitia in
+      consequuntur veniam cupiditate, rerum aut inventore? Recusandae iure
+      delectus perferendis quia incidunt rerum quibusdam accusamus repellat
+      quaerat illum ipsam maxime assumenda labore, quisquam nisi ab voluptates
+      reiciendis molestiae quasi nemo? Sunt dignissimos dolorum facere
+      accusantium exercitationem doloremque delectus dolor quasi. Voluptatibus
+      quis laboriosam facere tempora ut voluptates praesentium architecto cumque
+      provident? Sit ipsa cum nesciunt rem ex atque repellendus quis modi quia,
+      expedita iusto dolorum quasi vel earum. Doloremque optio et tenetur.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi vitae
+      placeat dolore minus sint voluptatem ipsam, unde officiis quisquam
+      possimus vel ea magnam necessitatibus dolores facilis mollitia in
+      consequuntur veniam cupiditate, rerum aut inventore? Recusandae iure
+      delectus perferendis quia incidunt rerum quibusdam accusamus repellat
+      quaerat illum ipsam maxime assumenda labore, quisquam nisi ab voluptates
+      reiciendis molestiae quasi nemo? Sunt dignissimos dolorum facere
+      accusantium exercitationem doloremque delectus dolor quasi. Voluptatibus
+      quis laboriosam facere tempora ut voluptates praesentium architecto cumque
+      provident? Sit ipsa cum nesciunt rem ex atque repellendus quis modi quia,
+      expedita iusto dolorum quasi vel earum. Doloremque optio et tenetur.
+    </p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+};
+</script>
